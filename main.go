b989b134f6754e8dbe8610c13fabbe2e01f4b265
taiki yamashita <taikity0808@gmail.com>
2021-10-20 10:07:23 +0900

    feature: port関数を定義

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"os"
 )
 
 const defaultport = "8080"
+
+func port() string {
+	p := os.Getenv("PORT")
+	if p != "" {
+		return ":" + p
+	}
+	return ":" + defaultport
+}
