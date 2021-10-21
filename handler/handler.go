179414237d32ee342ff5f19b2618ffafc9dc7959
taiki yamashita <taikity0808@gmail.com>
2021-10-21 09:29:43 +0900

    feature: New関数を作成

--- handler/handler.go
+++ handler/handler.go
@@ -12,6 +12,22 @@ type handler struct {
 	DB *gorm.DB
 }
 
+func New(db *gorm.DB) http.Handler {
+	e := echo.New()
+	e.Use(middleware.CORS())
+
+	h := &handler {
+		DB: db,
+	}
+
+	e.GET("/health", h.health)
+	e.GET("/todo", h.createTodo)
+	e.GET("/todo", h.getTodoLists)
+	e.DELETE("/todo/:id", h.deleteTodo)
+
+	return e
+}
+
 func (h *handler) health(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]string{"message": "OK"})
 }
